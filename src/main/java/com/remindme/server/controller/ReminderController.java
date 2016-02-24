777676707f9933ea0a19da00cc2265bb68feedee
Vitaliy Pugach <vitalick.pugach@gmail.com>
2016-02-24 13:22:38 +0200

    first server start

--- src/main/java/com/remindme/server/controller/ReminderController.java
+++ src/main/java/com/remindme/server/controller/ReminderController.java
@@ -1,15 +1,18 @@
-package com.base.controller;
+package com.remindme.server.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/remind")
+@RequestMapping("/reminder")
 public class ReminderController {
 
     @RequestMapping(value = "/get", method = RequestMethod.GET )
-    public String getReminder() {
+    @ResponseBody
+    public String getReminder(ModelMap model) {
         return "getReminder";
     }
 }
