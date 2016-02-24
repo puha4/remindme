5929d70d0a8d1b5ab100ee6decc58df12a330fd4
Yana <Yana>
2016-02-24 09:07:25 +0200

    start

---
+++ src/main/java/com/base/controller/ReminderController.java
@@ -0,0 +1,15 @@
+package com.base.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/remind")
+public class ReminderController {
+
+    @RequestMapping(value = "/get", method = RequestMethod.GET )
+    public String getReminder() {
+        return "getReminder";
+    }
+}
