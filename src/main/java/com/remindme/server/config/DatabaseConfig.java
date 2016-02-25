06288a91daf5a0313b5e9b0da8961786b947614a
Yana <Yana>
2016-02-25 08:16:22 +0200

    db configurate

---
+++ src/main/java/com/remindme/server/config/DatabaseConfig.java
@@ -0,0 +1,21 @@
+package com.remindme.server.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.annotation.Resource;
+
+@Configuration
+@EnableJpaRepositories("com.remindme.server.repository")
+@EnableTransactionManagement
+@PropertySource("classpath:db.properties")
+@ComponentScan("com.remindme.server")
+public class DatabaseConfig {
+
+    @Resource
+    private Environment env;
+}
