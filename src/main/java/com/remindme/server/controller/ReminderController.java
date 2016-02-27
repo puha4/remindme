f2496bb7a6f50d3bedbaa9865c44c5b2259133e3
Yana <Yana>
2016-02-27 13:38:10 +0200

    add json response converter

--- src/main/java/com/remindme/server/controller/ReminderController.java
+++ src/main/java/com/remindme/server/controller/ReminderController.java
@@ -1,18 +1,29 @@
 package com.remindme.server.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import com.remindme.server.entity.Remind;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Date;
+
+@RestController
 @RequestMapping("/reminder")
 public class ReminderController {
 
     @RequestMapping(value = "/get", method = RequestMethod.GET )
     @ResponseBody
-    public String getReminder(ModelMap model) {
-        return "getReminder";
+    public Remind getReminder() {
+        return createMockRemind();
+    }
+
+    private Remind createMockRemind() {
+        Remind remind = new Remind();
+        remind.setId(1);
+        remind.setTitle("My first remind");
+        remind.setRemindDate(new Date());
+
+        return remind;
     }
 }
